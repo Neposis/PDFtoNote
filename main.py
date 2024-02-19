430943146dd1f6878ff61f0a8806b8a883ecab9a
Neposis <davidscreation1@gmail.com>
2024-02-19 01:04:29 +0000

    Made code adjust to variable note sizes

--- main.py
+++ main.py
@@ -14,13 +14,16 @@ def open_pdfs():
 	
 	return pdfs
 
+def get_width(page):
+	media_box = page.mediabox
+	width = media_box.upper_right[0] - media_box.upper_left[0]
+	height = media_box.upper_right[1] - media_box.lower_right[1]
+	return width, height
+
 
 if __name__ == '__main__':
 	pdfs = open_pdfs()
 	lines = PdfReader("./Lined.pdf")
-	
-	right = Transformation().scale(0.5, 0.5).translate(650, 75)
-	left = Transformation().scale(0.75, 0.75).translate(-25.0, 75.0)
 	
 	lines = lines.pages[0]
 	
@@ -28,12 +31,19 @@ if __name__ == '__main__':
 		writer = PdfWriter()
 		reader = PdfReader("./in/{}".format(i))
 		
-		for p in reader.pages:
-			tmp_page = p
-			tmp_page.add_transformation(left)
-			tmp_page.merge_transformed_page(lines, right)
-			
-			writer.add_page(tmp_page)
+		width, height = get_width(reader.pages[0])
+		scalew = 500/width
+		scaleh = 590/height
+		scalef = min(scalew, scaleh)
+		print(scalew, scaleh)
+		right = Transformation().scale(0.5, 0.5).translate(500, 75)
+		left = Transformation().scale(scalef, scalef).translate(0, 150*(scaleh>1))
+		
+		for leftp in reader.pages:
+			tmpp = writer.add_blank_page(840, 590)
+			leftp.add_transformation(left)
+			tmpp.merge_page(leftp)
+			tmpp.merge_transformed_page(lines, right)
 		
 		writer.write("./out/{}".format(i))
 		
